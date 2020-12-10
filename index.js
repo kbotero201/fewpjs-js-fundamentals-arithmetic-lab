6d05f7a4c235350e450c57007a749b938b262177
kbotero201 <kbotero201@gmail.com>
2020-12-10 17:58:32 -0500

    done

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
 
 // Write your code here
+
+let num1 = 2
+let num2= 31
+
+let multiply = num1 * num2
+
+console.log(multiply)
+
+//
+
+let random = Math.floor(Math.random() * 1000) + 1
+
+console.log(random)
+
+//
+
+let mod = 16 % 6
+
+console.log(mod)
+//
+
+let max = Math.max(1,5,20)
+
+console.log(max)
